ae64569b139bb4c678edab295e132aad77a8b196
Rayla Kurosaki 『黒崎 レイラ』 <60267154+rkp1503@users.noreply.github.com>
2023-09-18 08:21:28 -0400

    Renamed file from "combos.py" to "dd_combos.py"

--- src/Archetypes/DD/dd_combos.py
+++ src/Archetypes/DD/dd_combos.py
@@ -8,20 +8,21 @@ from src.YuGiOh.Deck import Deck
 from src.Archetypes.DD.tournament_combos import one_card_combos
 
 
-def anime_combos(deck: Deck, local_database: dict) -> None:
+def anime_combos(local_database: dict) -> ComboCategory:
+    all_anime_combos: ComboCategory = ComboCategory("Anime Combos")
     # kings_combos(deck, local_database)
     # emperors_combos(deck, local_database)
     # super_doom_combos(deck, local_database)
-    return None
+    return all_anime_combos
 
 
-def tournament_combos() -> ComboCategory:
+def tournament_combos(local_database: dict) -> ComboCategory:
     all_tournament_combos: ComboCategory = ComboCategory("Tournament Combos")
     all_tournament_combos.add_combo(one_card_combos.all_one_card_combos())
     return all_tournament_combos
 
 
-def add_all_combos(deck: Deck) -> None:
-    # deck.add_combo_category(anime_combos())
-    deck.add_combo_category(tournament_combos())
+def add_all_combos(deck: Deck, local_database: dict) -> None:
+    deck.add_combo_category(anime_combos(local_database))
+    deck.add_combo_category(tournament_combos(local_database))
     return None
